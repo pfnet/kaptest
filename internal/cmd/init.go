9889fa40713d8a509dac4a7471346005de800fb0
piny940 <83708535+piny940@users.noreply.github.com>
2024-08-29 14:49:07 +0900

    separate init file

---
+++ internal/cmd/init.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const KAPTEST_DIR = ".kaptest/"
+
+var defaultManifest = `validatingAdmissionPolicies:
+  - # policy.yaml
+testSuites:
+  - policy: # policy-name
+    tests:
+      - object:
+          kind: Deployment
+          name: good-deployment
+        expect: admit
+      - object:
+          kind: Deployment
+          name: bad-deployment
+        expect: deny`
+
+func execInit(_ flags) {
+	dir := flag.Arg(1)
+	if !strings.HasSuffix(dir, "/") {
+		dir += "/"
+	}
+	dirInfo, err := os.Lstat(dir)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Stat(dir + KAPTEST_DIR)
+	if err != nil {
+		dirMode := dirInfo.Mode()
+		perm := dirMode & os.ModePerm
+		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
+			panic(fmt.Errorf("failed to make dir: %v", err))
+		}
+	}
+	if !file.IsDir() {
+		panic(fmt.Errorf("file %s already exists", dir+KAPTEST_DIR))
+	}
+	if file, err := os.Stat(dir + KAPTEST_DIR); err != nil || !file.IsDir() {
+		dirMode := dirInfo.Mode()
+		perm := dirMode & os.ModePerm
+		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
+			panic(fmt.Errorf("failed to make dir: %v", err))
+		}
+	}
+
+	f, err := os.Create(dir + KAPTEST_DIR + "kaptest.yaml")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.WriteString(defaultManifest)
+}
--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -22,7 +22,6 @@ import (
 	"log"
 	"log/slog"
 	"os"
-	"strings"
 )
 
 type flags struct {
@@ -68,59 +67,6 @@ func execRun(flags flags) {
 		}
 		log.Fatalf("Error: %v", err)
 	}
-}
-
-const KAPTEST_DIR = "kaptest/"
-
-var defaultManifest = `validatingAdmissionPolicies:
-  - # policy.yaml
-testSuites:
-  - policy: # policy-name
-    tests:
-      - object:
-          kind: Deployment
-          name: good-deployment
-        expect: admit
-      - object:
-          kind: Deployment
-          name: bad-deployment
-        expect: deny`
-
-func execInit(_ flags) {
-	dir := flag.Arg(1)
-	if !strings.HasSuffix(dir, "/") {
-		dir += "/"
-	}
-	dirInfo, err := os.Lstat(dir)
-	if err != nil {
-		panic(err)
-	}
-	file, err := os.Stat(dir + KAPTEST_DIR)
-	if err != nil {
-		dirMode := dirInfo.Mode()
-		perm := dirMode & os.ModePerm
-		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
-			panic(fmt.Errorf("failed to make dir: %v", err))
-		}
-	}
-	if !file.IsDir() {
-		panic(fmt.Errorf("file %s already exists", dir+KAPTEST_DIR))
-	}
-	if file, err := os.Stat(dir + KAPTEST_DIR); err != nil || !file.IsDir() {
-		dirMode := dirInfo.Mode()
-		perm := dirMode & os.ModePerm
-		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
-			panic(fmt.Errorf("failed to make dir: %v", err))
-		}
-	}
-
-	f, err := os.Create(dir + KAPTEST_DIR + "kaptest.yaml")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	f.WriteString(defaultManifest)
 }
 
 func usage() {
