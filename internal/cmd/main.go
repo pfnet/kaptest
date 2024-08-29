eda32b92edf28f47c4a75b0881668b01a1c00e2a
piny940 <83708535+piny940@users.noreply.github.com>
2024-08-29 15:03:34 +0900

    move execInit to main.go

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -22,6 +22,7 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"strings"
 )
 
 type flags struct {
@@ -36,6 +37,7 @@ func main() {
 	flag.BoolVar(&cfg.Verbose, "verbose", false, "Verbose output")
 	flag.Parse()
 	command := flag.Arg(0)
+	initLog(cfg)
 
 	switch command {
 	case "run":
@@ -46,6 +48,59 @@ func main() {
 		usage()
 		os.Exit(1)
 	}
+}
+
+const KAPTEST_DIR = ".kaptest/"
+
+var defaultManifest = `validatingAdmissionPolicies:
+  -  # policy.yaml
+testSuites:
+  - policy: # policy-name
+    tests:
+      - object:
+          kind: Deployment
+          name: good-deployment
+        expect: admit
+      - object:
+          kind: Deployment
+          name: bad-deployment
+        expect: deny
+`
+
+func execInit(_ flags) {
+	dir := flag.Arg(1)
+	if !strings.HasSuffix(dir, "/") {
+		dir += "/"
+	}
+	dirInfo, err := os.Lstat(dir)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Stat(dir + KAPTEST_DIR)
+	if err != nil {
+		dirMode := dirInfo.Mode()
+		perm := dirMode & os.ModePerm
+		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
+			panic(fmt.Errorf("failed to make dir: %v", err))
+		}
+	} else if !file.IsDir() {
+		panic(fmt.Errorf("file %s already exists", dir+KAPTEST_DIR))
+	}
+	if file, err := os.Stat(dir + KAPTEST_DIR); err != nil || !file.IsDir() {
+		dirMode := dirInfo.Mode()
+		perm := dirMode & os.ModePerm
+		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
+			panic(fmt.Errorf("failed to make dir: %v", err))
+		}
+	}
+
+	f, err := os.Create(dir + KAPTEST_DIR + "kaptest.yaml")
+	if err != nil {
+		panic(fmt.Errorf("failed to create kaptest.yaml: %v", err))
+	}
+	defer f.Close()
+
+	f.WriteString(defaultManifest)
 }
 
 func execRun(flags flags) {
@@ -59,8 +114,6 @@ func execRun(flags flags) {
 		return
 	}
 
-	initLog(cfg)
-
 	if err := tester.Run(cfg); err != nil {
 		if errors.Is(err, tester.ErrTestFail) {
 			os.Exit(1)
@@ -71,13 +124,13 @@ func execRun(flags flags) {
 
 func usage() {
 	fmt.Printf(`Usage
-  run <file> : run test
   init <dir> : setup kaptest in <dir>
+  run <file> : run test
 	`)
 }
 
-func initLog(cfg tester.CliConfig) {
-	if cfg.Debug {
+func initLog(flags flags) {
+	if flags.Debug {
 		slog.SetLogLoggerLevel(slog.LevelDebug)
 	}
 }
