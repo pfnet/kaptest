57ab9e130f155501b1147ebeaf2a6a1f0c139ef5
piny940 <83708535+piny940@users.noreply.github.com>
2024-08-29 14:46:28 +0900

    feat: kaptest init

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -22,6 +22,7 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	"strings"
 )
 
 type flags struct {
@@ -69,14 +70,63 @@ func execRun(flags flags) {
 	}
 }
 
-func execInit(flags flags) {
+const KAPTEST_DIR = "kaptest/"
 
+var defaultManifest = `validatingAdmissionPolicies:
+  - # policy.yaml
+testSuites:
+  - policy: # policy-name
+    tests:
+      - object:
+          kind: Deployment
+          name: good-deployment
+        expect: admit
+      - object:
+          kind: Deployment
+          name: bad-deployment
+        expect: deny`
+
+func execInit(_ flags) {
+	dir := flag.Arg(1)
+	if !strings.HasSuffix(dir, "/") {
+		dir += "/"
+	}
+	dirInfo, err := os.Lstat(dir)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Stat(dir + KAPTEST_DIR)
+	if err != nil {
+		dirMode := dirInfo.Mode()
+		perm := dirMode & os.ModePerm
+		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
+			panic(fmt.Errorf("failed to make dir: %v", err))
+		}
+	}
+	if !file.IsDir() {
+		panic(fmt.Errorf("file %s already exists", dir+KAPTEST_DIR))
+	}
+	if file, err := os.Stat(dir + KAPTEST_DIR); err != nil || !file.IsDir() {
+		dirMode := dirInfo.Mode()
+		perm := dirMode & os.ModePerm
+		if err := os.Mkdir(dir+KAPTEST_DIR, perm); err != nil {
+			panic(fmt.Errorf("failed to make dir: %v", err))
+		}
+	}
+
+	f, err := os.Create(dir + KAPTEST_DIR + "kaptest.yaml")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.WriteString(defaultManifest)
 }
 
 func usage() {
 	fmt.Printf(`Usage
   run <file> : run test
-  init <dir> : create kaptest manifest in <dir>
+  init <dir> : setup kaptest in <dir>
 	`)
 }
 
