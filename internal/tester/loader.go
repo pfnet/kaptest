5d9a7c74a4e034a1704730cfd3bc92674114eee5
Naoki MATSUMOTO <nmatsumoto@preferred.jp>
2026-07-01 09:53:50 +0900

    MAP: fix defaulting
    These refelects defalut values
    MatchPolicy: Equivalent
    NamespaceSelector: emtpty LabelSelector
    ObjectSelector: empty LabelSelector

--- internal/tester/loader.go
+++ internal/tester/loader.go
@@ -108,8 +108,8 @@ func (r *ResourceLoader) LoadPolicies(paths []string) {
 					continue
 				}
 				m := obj.(*v1beta1.MutatingAdmissionPolicy)
-				// Ensure nil labelSelectors to be matching everything
-				defaultingMAPPolicy(m)
+				// Ensure matchConstraints to be defaulted
+				defaultingMAP(m)
 				r.Maps[m.Name] = m
 			default:
 				slog.Warn("unexpected manifest", "kind", gvk.Kind)
@@ -181,11 +181,15 @@ func (r *ResourceLoader) GetResource(ngvk NameWithGVK) (*unstructured.Unstructur
 	return obj, nil
 }
 
-func defaultingMAPPolicy(p *v1beta1.MutatingAdmissionPolicy) {
+func defaultingMAP(p *v1beta1.MutatingAdmissionPolicy) {
 	// MAP's matcher recognizes nil as labels.Nothing
 	// To match everything as expected, it needs to set empty LabelSeletor
 	// Ref: https://github.com/kubernetes/apiserver/blob/v0.32.1/pkg/admission/plugin/policy/generic/policy_matcher.go#L96
 
+	if p.Spec.MatchConstraints.MatchPolicy == nil {
+		eq := v1beta1.Equivalent
+		p.Spec.MatchConstraints.MatchPolicy = &eq
+	}
 	if p.Spec.MatchConstraints.NamespaceSelector == nil {
 		p.Spec.MatchConstraints.NamespaceSelector = &metav1.LabelSelector{}
 	}
