6b03b18b28c5d15b89a581da19dd9c775fccb42f
Naoki MATSUMOTO <nmatsumoto@preferred.jp>
2026-08-17 18:40:33 +0900

    MAP: apply defalut values to the expected object
    MAP apply default values to the mutated object.
    This patch apply defalut values to the expected object,
    and no more default values are required.

--- internal/tester/tester_test.go
+++ internal/tester/tester_test.go
@@ -44,6 +44,7 @@ func TestRun(t *testing.T) {
 				"./testdata/map-with-userinfo.test/kaptest.yaml",
 				"./testdata/vap-defaulting.test/kaptest.yaml",
 				"./testdata/map-defaulting.test/kaptest.yaml",
+				"./testdata/map-object-defaults.test/kaptest.yaml",
 			},
 			wantErr:           nil,
 			validateManifests: true,
